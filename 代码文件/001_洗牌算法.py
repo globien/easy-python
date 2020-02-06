c87a1911297f05f03d69717e615f053f32f5dcb4
globien <56852450+globien@users.noreply.github.com>
2020-02-05 19:28:41 -0500

    Update 001_洗牌算法.py

--- 代码文件/001_洗牌算法.py
+++ 代码文件/001_洗牌算法.py
@@ -1,10 +1,14 @@
 import random
-TOTAL_NUMBER = 54                           # 牌的总张数
-DEAL_NUMBER = 54                            # 发牌张数，等于牌的总张数时相当于洗牌
-list1 = [i+1 for i in range(TOTAL_NUMBER)]  # 初始化原牌堆，方法无所谓，完整而不重复即可
-list2 = []                                  # 新牌堆开始时为空
+TOTAL_NUMBER = 54                               # 牌的总张数
+DEAL_NUMBER = 54                                # 发牌张数，等于牌的总张数时相当于洗牌
+list1 = [i+1 for i in range(TOTAL_NUMBER)]      # 初始化原牌堆，方法无所谓，完整而不重复即可
+list2 = []                                      # 新牌堆开始时为空
 for i in range(DEAL_NUMBER):
- list2.append(random.choice(list1))         # 从原牌堆中随机抽取一张牌放到新牌堆中
- list1.remove(list2[i])                     # 从原牌堆中删除刚才抽到的那张牌
-print("A New Card List Produced:")
-print(list2)
+    list2.append(random.choice(list1))          # 从原牌堆中随机抽取一张牌放到新牌堆中
+    list1.remove(list2[i])                      # 从原牌堆中删除刚才抽到的那张牌
+print("A new card list produced:\n",list2)
+
+# Shuffle directly by random.shuffle function
+# list1 = [i+1 for i in range(TOTAL_NUMBER)]
+# random.shuffle(list1)
+# print("Shuffle directly by random.shuffle function:\n",list1)

30ad544fdc06fb51e31d755adc7e26c3db3524d5
globien <56852450+globien@users.noreply.github.com>
2020-02-07 16:26:56 -0500

    Update 003a_蒙特卡洛法计算圆周率π.py

--- 代码文件/003a_蒙特卡洛法计算圆周率π.py
+++ 代码文件/003a_蒙特卡洛法计算圆周率π.py
@@ -1,20 +1,20 @@
 import random
 
-num_all = 0
-num_cir = 0
-num_halt = 10000000                                  #每循环该次数后，打印一次目前的计算结果
+num_all = 0         #随机点总计数器
+num_cir = 0         #随机点在圆内的计数器
+num_halt = 10000000 #每产生这么多个随机点后，计算并打印一次目前的结果
 
 print("将进行无限计算，请用Ctrl_C或其他方式强制退出！！！")
-input("按任意键开始...")
+input("按回车(Enter)键开始...")
 print("开始计算...，退出请用Ctrl_C或其他强制退出方式...")
 print("\n实验次数        计算结果")
 
 while 1 :
-    for i in range(num_halt):
-        x = random.random()                         #随机获得横坐标
-        y = random.random()                         #随机获得纵坐标
-        if x*x + y*y < 1 :                          #点(x,y)在圆内
-            num_cir = num_cir + 1
-        num_all = num_all + 1
+    for i in range(num_halt): 
+        x = random.random()         #获得随机点的横坐标
+        y = random.random()         #获得随机点的纵坐标
+        if x*x + y*y < 1 :          #随机点(x,y)在圆内
+            num_cir = num_cir + 1   #圆内计数器+1
+        num_all = num_all + 1       #总计数器+1
     pi = 4*num_cir/num_all
     print(num_all,"   ", pi)

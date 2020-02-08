2161e2b86dde2e50e52f7becb53497dbb50a1237
globien <56852450+globien@users.noreply.github.com>
2020-02-08 10:49:21 -0500

    Update 003b_用割圆法计算圆周率π.py

--- 代码文件/003b_用割圆法计算圆周率π.py
+++ 代码文件/003b_用割圆法计算圆周率π.py
@@ -6,8 +6,9 @@ import math
 
 n=6
 a=1
-print(n,"     ",n*a/2)
+print("\n内接正n边形        π计算结果")
+print(n,"           ",n*a/2)
 for i in range(15) :
   n=2*n
   a=math.sqrt(2-2*math.sqrt(1-(a/2)**2))
-  print(n,"     ",n*a/2)
+  print(n,"          ",n*a/2)

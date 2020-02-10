2b73e3f859fe5f631d099135cd701aad5424c923
globien <56852450+globien@users.noreply.github.com>
2020-02-10 15:15:55 -0500

    Update 001_Python基本语句示例.py

--- 代码文件/001_Python基本语句示例.py
+++ 代码文件/001_Python基本语句示例.py
@@ -1,18 +1,18 @@
-#https://github.com/globien/easy_python/blob/master/描述文件/001_Python易懂不用学.md
+#https://github.com/globien/easy_python/blob/master/描述文件/001_Python不用学.md
 #Python编程的基本语句示例
 #下面的引号用单引号双引号均可，但要成对使用，且注意不要用中文引号
 
 print("Hello World!")       
-print(3+4)
-print("5 除以 3 等于", 5/3)
-print()                               #打印一个空行
+print(2+3)
+print("5 + 3 =", 5/3)
 
 x = 4
 y = 3
-print("x * y =", x*y)
-a = x**y
-print(x, "的", y, "次方等于", a)
-print()
+a = x*y
+b = x**y
+print("a =", a)
+print("b =", b)
 
-name = "小强"
-print("你好,",name,"!")
+first_name = "Harry"
+last_name = "Potter"
+print("Hi", first_name, last_name, "!")

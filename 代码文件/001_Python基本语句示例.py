ab82928a3b7d9b65d471dc33c4ec42009809e23c
globien <56852450+globien@users.noreply.github.com>
2020-02-12 19:45:08 -0500

    Update 001_Python基本语句示例.py

--- 代码文件/001_Python基本语句示例.py
+++ 代码文件/001_Python基本语句示例.py
@@ -1,6 +1,5 @@
-#https://github.com/globien/easy_python/blob/master/描述文件/001_Python不用学.md
-#Python编程的基本语句示例
-#下面的引号用单引号双引号均可，但要成对使用，且注意不要用中文引号
+# Python编程的基本语句示例
+# 下面的引号用单引号双引号均可，但要成对使用且注意不要用中文引号
 
 print("Hello World!")       
 print(2+3)
@@ -19,3 +18,5 @@ print("c =", c)
 first_name = "Harry"
 last_name = "Potter"
 print("Hi", first_name, last_name, "!")
+
+# 说明文件：https://github.com/globien/easy_python/blob/master/描述文件/001_Python不用学.md

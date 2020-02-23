10a96b89c0e78029c7bdf5a23440631830077587
globien <56852450+globien@users.noreply.github.com>
2020-02-23 11:31:43 -0500

    Update 102_抛硬币连续正面问题.py

--- 代码文件/102_抛硬币连续正面问题.py
+++ 代码文件/102_抛硬币连续正面问题.py
@@ -2,26 +2,27 @@
 
 import random
 
-EXP_NUM = 150000    # 总的抛硬币次数
 N = 3               # 我们将观察连续出现N次正面后会发生什么
+EXP_NUM = 150000    # 总的抛硬币次数
 
-counter1 = 0        # 用来统计是否连续正面
-counter2 = 0        # 统计共多少次出现连续正面
-counter3 = 0        # 统计出现连续正面后，再一次出现正面的次数
+counter1 = 0        # 用来统计已经连续几次正面了
+counter2 = 0        # 统计共多少次出现连续N次正面
+counter3 = 0        # 统计出现连续N次正面后，再一次出现正面的次数
 
 for i in range(EXP_NUM):
     coin = random.randint(0,1)
-    if coin == 0:
-        counter1 = 0                    # 不是正面，重置
+    if coin == 1:                       # 扔到了一次正面
+        counter1 = counter1 + 1         # 累计出现正面的次数          
     else:
-        counter1 = counter1 + 1         # 累计出现正面的次数
+        counter1 = 0                    # 不是正面，重置计数器        
     
     if counter1 == N:                   # 表明连续出现了N次正面
-        counter2 = counter2 + 1         
-        counter3 = counter3 + random.randint(0,1)  # 再扔一次
-        counter1 = 0                    # 重置
+        counter2 = counter2 + 1         # 累计出现N次正面的次数
+        counter3 = counter3 + random.randint(0,1)  # 再扔一次，累计连续N次正面后再次正面的次数
+        counter1 = 0                    # 重置计数器，准备下一次实验
         
 print("N =", N)
+print("EXP_NUM = ", EXP_NUM)
 print("连续出现N次正面的总次数", counter2)
 print("连续出现N次正面后，再次出现正面的次数", counter3)
 print("连续出现N次正面后，再次出现正面的比例", counter3/counter2)

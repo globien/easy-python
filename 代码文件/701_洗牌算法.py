000a9665d46835ff67a6f4c314252899fdf7cf18
globien <56852450+globien@users.noreply.github.com>
2020-02-23 10:19:00 -0500

    Update 701_洗牌算法.py

--- 代码文件/701_洗牌算法.py
+++ 代码文件/701_洗牌算法.py
@@ -1,3 +1,5 @@
+# 一种简单高效的洗牌方法
+
 import random
 TOTAL_NUMBER = 54                               # 牌的总张数
 DEAL_NUMBER = 54                                # 发牌张数，等于牌的总张数时相当于洗牌
@@ -8,7 +10,11 @@ for i in range(DEAL_NUMBER):
     list1.remove(list2[i])                      # 从原牌堆中删除刚才抽到的那张牌
 print("A new card list produced:\n",list2)
 
-# Shuffle directly by random.shuffle function
-# list1 = [i+1 for i in range(TOTAL_NUMBER)]
-# random.shuffle(list1)
-# print("Shuffle directly by random.shuffle function:\n",list1)
+# 直接使用Python的shuffle函数洗牌
+'''
+list1 = [i+1 for i in range(TOTAL_NUMBER)]
+random.shuffle(list1)
+print("Shuffle directly by random.shuffle function:\n",list1)
+'''
+
+# 详述文件: https://github.com/globien/easy_python/blob/master/描述文件/104_如何高效而完美地洗牌.md

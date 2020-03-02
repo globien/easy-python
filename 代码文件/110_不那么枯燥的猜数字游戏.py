b5bab9a112fe447383ee28ba88dd13a7973bf71f
globien <56852450+globien@users.noreply.github.com>
2020-03-02 11:12:37 -0500

    Update 110_不那么枯燥的猜数字游戏.py

--- 代码文件/110_不那么枯燥的猜数字游戏.py
+++ 代码文件/110_不那么枯燥的猜数字游戏.py
@@ -5,21 +5,21 @@ import random
 very_happy = "小蟒今天开心极了，你来猜猜我的心情指数？"
 happy = "小蟒现在挺高兴，你猜猜我的心情指数是多少？"
 so_so = "小蟒现在感觉还行吧，你想知道我的心情指数是多少吗？"
-sad = "小蟒不太高兴，你猜猜我的心情指数吧。"
-very_sad = "小蟒今天不开心，要猜你就猜小一点的数字吧。"
-level_list = [very_happy, happy, so_so, sad, very_sad]
+sad = "小蟒现在不太高兴，你猜猜我的心情指数吧。"
+very_sad = "小蟒今天很不开心，要猜你就猜小一点的数字吧。"
+level_list = [very_sad, sad, so_so, happy, very_happy]
 
 level = random.randint(0,4)
 emotion = level * 20 + random.randint(1,20)
-print(level_list[level])
+print(level, level_list[level], emotion)
 
 guess = int(input("请输入1~100的数字："))
 n = 1                       # 已经猜的次数
 while guess != emotion:
     if guess == 0: exit()
     if guess - emotion > 10: print("你猜得太大了！！！")
-    elif guess - emoiton > 0: print("大了一点点，加油！")
-    elif guess - emoiton < -10: print("你猜得太小了！！！")
+    elif guess - emotion > 0: print("大了一点点，加油！")
+    elif guess - emotion < -10: print("你猜得太小了！！！")
     else: print("猜得小了一点，继续！")
     guess = int(input("请重新输入你的猜测："))
     n = n + 1

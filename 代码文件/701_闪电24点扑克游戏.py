986b6744eda161c1cf58d61c9961a7e719e1d728
globien <56852450+globien@users.noreply.github.com>
2020-03-03 22:03:16 -0500

    Update 701_闪电24点扑克游戏.py

--- 代码文件/701_闪电24点扑克游戏.py
+++ 代码文件/701_闪电24点扑克游戏.py
@@ -3,18 +3,38 @@
 from random import *                        # 导入随机函数
 from itertools import *                     # 导入概率统计函数
 
+# 定义给表达式加括号的子程序
+def add_括号(exp, n)
+    if n > 0:
+        exp.insert(0,'(')
+        exp.insert(4,')')
+        return exp
+    
+
+# 定义计算24点的子程序
+def find_solution(cards, signs):
+    for i in range(24):
+        for j in range(24):
+            exp = cards[i][0]+signs[j][0]+cards[i][1]+signs[j][1]+cards[i][2]+signs[j][2]+cards[i][3]
+            if abs(eval(exp)-24) < 0.000001 :
+                print("Find a solution: ", exp)
+                return
+            else:
+                for i in range(7):
+                    exp_括号 = add_括号(exp,i)
+                    if abs(eval(exp_括号)-24) < 0.000001
+                    print("Find a solution: ", exp_括号)
+                    return
+
 cards_all = [i for i in range(1,11) for j in range(16 if i==1 else 4)]
-
-
 shuffle(cards_all)                                  # 洗牌
 cards_four = [str(cards_all[i]) for i in range(4)]  # 发四张牌
+cards_four = ["3", "8", "1", "1"]
 print(cards_four)
 cards = list(permutations(cards_four,4))
 signs = list(permutations(["+","-","*","/"],3))
 
-for j in range(5):
-    exp = cards[0][0]+signs[j][0]+cards[0][1]+signs[j][1]+cards[0][2]+signs[j][2]+cards[0][3]
-    print(eval(exp))
+find_solution(cards, signs)
 
 
 

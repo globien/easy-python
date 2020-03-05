0ee336e78ea07c283193a5916a8e175a72ccd3ef
globien <56852450+globien@users.noreply.github.com>
2020-03-04 19:42:04 -0500

    Update 701_闪电24点扑克游戏.py

--- 代码文件/701_闪电24点扑克游戏.py
+++ 代码文件/701_闪电24点扑克游戏.py
@@ -6,15 +6,16 @@ from itertools import *                     # 导入概率统计函数
 # 定义计算24点的子程序
 def find_solution(cards, signs):
     for i in range(24):
-        for j in range(24):
+        for j in range(64):
             exp_list = [cards[i][0], signs[j][0], cards[i][1], signs[j][1], cards[i][2], signs[j][2], cards[i][3] ]
             exp = "".join(exp_list)       # 把列表合并为字符串
-            print(i, j, exp)
+            # print(i, j, exp)
             if abs(eval(exp)-24) < 0.000001 :
                 print("Found a solution: ", exp)
                 return 1
+            '''    
             else:
-                # exp_list = ["8","/","3","-","8","/","3"]
+                exp_list = ["8","/","3","-","8","/","3"]
                 for k in range(6):
                     exp_list_pare = add_pare(exp_list,k)
                     exp_pare = "".join(exp_list_pare)
@@ -24,7 +25,7 @@ def find_solution(cards, signs):
                     if abs(result-24) < 0.000001:
                         print("Found a solution: ", exp_pare)
                         return 1
-
+            '''
     return 0
 
 # 定义给表达式加括号的子程序
@@ -64,7 +65,7 @@ cards_four = [str(cards_all[i]) for i in range(4)]  # 发四张牌
 cards_four = ["8", "8", "3", "3"]
 print(cards_four)
 cards = list(permutations(cards_four,4))
-signs = list(permutations(["+","-","*","/"],3))
+signs = list(product(["+","-","*","/"],repeat=3))
 
 if find_solution(cards, signs) == 0:
     print("There is no solution.")

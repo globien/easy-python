796f4fdd18e22458926754175307f04146018253
globien <56852450+globien@users.noreply.github.com>
2020-03-05 13:31:28 -0500

    Create 901_计算某日期是一年中的第几天.py

---
+++ 代码文件/901_计算某日期是一年中的第几天.py
@@ -0,0 +1,10 @@
+year = int(input("Year: "))
+month = int(input("Month: "))
+day = int(input("Day: "))
+
+days = [31,28,31,30,31,30,31,31,30,31,30]  # 前11月的，12月的没用
+for i in range(month-1):
+    day = day + days[i]
+if year%4 == 0 and year%100 != 0 and month > 2:
+    day = day +1
+print("它是第", day, "天")

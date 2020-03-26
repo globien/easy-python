dc9d452891b2cbb084e72e34bb152a4ea4e5883c
globien <56852450+globien@users.noreply.github.com>
2020-03-25 22:44:11 -0400

    Update 202_polynomial-simulation.py

--- 代码文件/202_polynomial-simulation.py
+++ 代码文件/202_polynomial-simulation.py
@@ -1,22 +1,24 @@
 import numpy as np
 from matplotlib import pyplot as plt
-num_cases = [115, 142, 198, 235, 343, 436, 596, 727, 873, 1087, 1330, 1470, 2091, 2792]   # number of cases in order of dates
+
+# number of cases in order of dates
+num_cases = [115, 142, 198, 235, 343, 436, 596, 727, 873, 1087, 1330, 1470, 2091, 2792]
 
 days = len(num_cases)                  # count the days input with the number of cases
-x = [ i+1 for i in range(days) ]         # x coordinate, in days
-y = num_cases                             # y coordinate, i.e., number of cases for each day
-plt.scatter(x, y)                             # print the original numbers in dots
+x = [ i+1 for i in range(days) ]       # x coordinate, in days
+y = num_cases                          # y coordinate, i.e., number of cases for each day
+plt.scatter(x, y)                      # print the original numbers in dots
 
 # simulate with polynomial of degree 3
 parameters = np.polyfit(x, y, 3)
 func = np.poly1d(parameters)
 
-# predict for more days
-days_extended = 14                          
-for i in range(days_extended):                  
-    x.append(days+i+1)              
+# extend for more days and draw the curve
+days_extend = 14                          
+for i in range(days_extend):
+  x.append(days+i+1)
 plt.plot(x, func(x), color='g')
 plt.xlabel('days')
 plt.ylabel('number of cases')
-plt.title('A rough prediction of the number of cases by simulation')
+plt.title('A Rough Prediction of the Number of Cases by Simulation')
 plt.show()

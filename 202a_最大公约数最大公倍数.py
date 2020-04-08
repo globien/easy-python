1d2cb4dafa44d81111216e540a69206e8ddbf936
henryjiang <henryjiang@me.com>
2020-04-07 20:41:32 -0400

    New code file

---
+++ 202a_最大公约数最大公倍数.py
@@ -0,0 +1,16 @@
+def greatest_common_divisor(m,n):
+    while m!=n:
+        if m > n :
+            m = m - n
+        else:
+            n = n - m
+    return m
+
+def least_common_multiple(m,n):
+    return m * n // greatest_common_divisor(m,n)
+
+if __name__ == "__main__" :
+    m,n = eval(input('请输入两个正整数（逗号隔开）：'))
+    print('m =',m, ', n =',n)
+    print('它们的最大公约数是：', greatest_common_divisor(m,n))
+    print('它们的最小公倍数是：', least_common_multiple(m,n))
---
+++ 202b_数学之美太阳花.py
@@ -0,0 +1,24 @@
+import turtle
+from math import *
+from 202a_最大公约数最大公倍数 import *
+
+r = 25
+R = 120
+d = 50
+
+flower = turtle.Turtle()
+flower.penup()
+flower.color("orange")
+flower.pensize("2")
+flower.begin_fill()
+theta = 0
+while theta < 2 * pi * least_common_multiple(r, R)/R:
+    x = (R-r) * cos(theta) + d * cos((R - r) / r * theta)
+    y = (R-r) * sin(theta) - d * sin((R - r) / r * theta)
+    flower.goto(x, y)
+    flower.pendown()
+    theta += 0.05
+flower.hideturtle()
+flower.end_fill()
+turtle.done()
+
